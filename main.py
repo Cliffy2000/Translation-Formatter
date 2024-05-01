06d95bfcf70cce1d246fc7e54f6360de75a44d06
Cliff <cliffyyc@outlook.com>
2024-05-01 21:02:35 +0800

    Fixes for load file UI and gitignore

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import sys
+import time
 import pandas as pd
 from PyQt6 import QtWidgets
 from PyQt6.QtGui import QFont
+
+
+application = QtWidgets.QApplication(sys.argv)
+font = QFont()
+font.setPointSize(12)
+application.setFont(font)
+
 
 class Application(QtWidgets.QMainWindow):
     def __init__(self):
@@ -16,22 +24,35 @@ class Application(QtWidgets.QMainWindow):
 
 
     def selectFile(self):
-        fileName, _ = QtWidgets.QFileDialog.getOpenFileName(self, "Select File")
-        if fileName:
+        file, _ = QtWidgets.QFileDialog.getOpenFileName(self, "Select File")
+        if file:
+            self.fileSelectButton.hide()
+            if hasattr(self, "fileNameLabel"):
+                self.fileNameLabel.setText("Loading file...")
+            else:
+                self.fileNameLabel = QtWidgets.QLabel("Loading file...", self)
+                self.fileNameLabel.setGeometry(50, 50, 150, 50)
+                self.fileNameLabel.show()
+            QtWidgets.QApplication.processEvents()
+
+            fileName = file.split('/')[-1]
+            loadExcelFile(fileName)
+            self.fileNameLabel.setText(fileName)
+
             self.fileSelectButton.setGeometry(250, 50, 130, 50)
+            self.fileSelectButton.show()
             self.fileSelectButton.setText("Reselect File")
 
-            self.fileNameLabel = QtWidgets.QLabel(fileName.split('/')[-1], self)
-            self.fileNameLabel.setGeometry(50, 50, 150, 50)
-            self.fileNameLabel.show()
+
+def loadExcelFile(f):
+    sheetDict = pd.read_excel(f)
+    for k in sheetDict.keys:
+        print(k)
+    return
 
 
-application = QtWidgets.QApplication(sys.argv)
-font = QFont()
-font.setPointSize(12)
-application.setFont(font)
+if __name__ == "__main__":
+    window = Application()
+    window.show()
 
-window = Application()
-window.show()
-
-sys.exit(application.exec())
+    sys.exit(application.exec())
